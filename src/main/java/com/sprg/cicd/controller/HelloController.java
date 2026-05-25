6feb1f2a4e09f4955f09ff6ec955428d8a524752
Shailesh Patil <shaileshofficial@2024gmail.com>
2026-05-25 15:39:45 +0000

    Initial commit - Spring Boot CI/CD project

---
+++ src/main/java/com/sprg/cicd/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sprg.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "CI/CD Pipeline Success";
+    }
+}
